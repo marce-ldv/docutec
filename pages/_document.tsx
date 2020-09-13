110bfae70cfead4a98fbd226665bd8b2f1e0ecfd
MarceloLudovino <marcelo.docutec@gmail.com>
2020-09-13 20:14:09 -0300

    Add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,6 +21,7 @@ class MyDocument extends Document {
 					<meta name="google-site-verification" content="40xpsLPhX8g30ImASZvllemNkbwj05_GGTVbqiabX8s"/>
 					<meta name="msvalidate.01" content="8374C2018616D0808646CCCAC5E579A7"/>
 					<meta name="description" content="Servicio tecnico de reparacion y manimiento de impresoras y fotocopiadoras."/>
+					<link rel="icon" href="/assets/favicon.ico" type="image/x-icon"/>
 					<title>Docutec Mdp</title>
 				</Head>
 				<body>
