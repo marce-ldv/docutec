f6d9b262ec8efc59d44a6141c58e6b9b7d5fc055
MarceloLudovino <ext-m.ludovino@rappi.com>
2020-09-15 15:11:19 -0300

    generate icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -39,37 +39,44 @@ class MyDocument extends Document {
 					<meta name='mobile-web-app-capable' content='yes'/>
 					<meta name='theme-color' content='#FFFFFF'/>
 
-					<link rel='manifest' href='/manifest.json'/>
-					<link rel='shortcut icon' href='/assets/favicon.ico'/>
-					<link rel='apple-touch-icon' sizes='180x180' href='/assets/logo.png' />
-					<link rel='apple-touch-icon' sizes='180x180' href='/assets/logo.webp' />
-					<link rel='icon' type='image/png' sizes='32x32' href='/assets/logo.png' />
-					<link rel='icon' type='image/png' sizes='32x32' href='/assets/logo.webp' />
-					<link rel='icon' type='image/png' sizes='16x16' href='/assets/logo.png' />
-					<link rel='icon' type='image/png' sizes='16x16' href='/assets/logo.webp' />
-					<link rel='mask-icon' href='/assets/logo.webp' color='#5bbad5' />
-					<link rel='mask-icon' href='/assets/logo.png' color='#5bbad5' />
-					<link rel='shortcut icon' href='/assets/favicon.ico' />
-					<link rel='shortcut icon' href='/assets/favicon.png' />
+					<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
+					<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
+					<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
+					<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
+					<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
+					<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
+					<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
+					<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
+					<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
+					<link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png"/>
+					<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+					<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
+					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+					<link rel="manifest" href="/manifest.json"/>
+					<meta name="msapplication-TileColor" content="#ffffff"/>
+					<meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
+					<meta name="theme-color" content="#ffffff"/>
 
 
-					<meta name='twitter:card' content='summary' />
-					<meta name='twitter:url' content='https://docutec.vercel.app' />
-					<meta name='twitter:title' content='Docutec Mdp' />
-					<meta name='twitter:description' content='Servicio tecnico de reparacion y manimiento de impresoras y fotocopiadoras.' />
-					<meta name='twitter:image' content='https://docutec.vercel.app/static/icons/android-chrome-192x192.png' />
-					<meta name='twitter:creator' content='@DavidWShadow' />
-					<meta property='og:type' content='website' />
-					<meta property='og:title' content='Docutec Mdp' />
-					<meta property='og:description' content='Servicio tecnico de reparacion y manimiento de impresoras y fotocopiadoras.' />
-					<meta property='og:site_name' content='PWA App' />
-					<meta property='og:url' content='https://docutec.vercel.app' />
-					<meta property='og:image' content='https://docutec.vercel.app/static/icons/apple-touch-icon.png' />
-					
+					<meta name='twitter:card' content='summary'/>
+					<meta name='twitter:url' content='https://docutec.vercel.app'/>
+					<meta name='twitter:title' content='Docutec Mdp'/>
+					<meta name='twitter:description'
+								content='Servicio tecnico de reparacion y manimiento de impresoras y fotocopiadoras.'/>
+					<meta name='twitter:image' content='https://docutec.vercel.app/static/icons/android-chrome-192x192.png'/>
+					<meta name='twitter:creator' content='@docutec'/>
+					<meta property='og:type' content='website'/>
+					<meta property='og:title' content='Docutec Mdp'/>
+					<meta property='og:description'
+								content='Servicio tecnico de reparacion y manimiento de impresoras y fotocopiadoras.'/>
+					<meta property='og:site_name' content='Docutec'/>
+					<meta property='og:url' content='https://docutec.vercel.app'/>
+					<meta property='og:image' content='https://docutec.vercel.app/static/icons/apple-touch-icon.png'/>
+
 				</Head>
 				<body>
 				<Main/>
-				<NextScript />
+				<NextScript/>
 				</body>
 			</Html>
 		)
